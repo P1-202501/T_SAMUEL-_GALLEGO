929a1b4f667804b923c6d8d378293820e11ce7c7
sammygapaCES <gallegop.samuel@uces.edu.co>
2025-05-14 07:46:21 -0500

    comit 3 elim

--- Taller.py
+++ Taller.py
@@ -4,11 +4,29 @@ import pandas as pd
 df = pd.read_csv('biometria_pacientes.csv', sep=';', encoding='latin-1')
 
 
-#2. Identificar duplicados, nulos y valores únicos
+#2. identificar duplicados, nulos y valores únicos
 # Dupli
 print(f"Número de duplicados:{df.duplicated().sum()}")
 
-# Nulos por colum
+# nulos por colum
 print(df.isnull().sum())
 # Valores únicos en 'Fuma'
 print(df['Fuma'].unique())
+
+
+# 3. eliminar duplicados por columnas criticas
+df = df.drop_duplicates()
+
+columnas_criticas = ['Peso', 'Talla', 'Glucosa', 'Colesterol']
+df = df.dropna(subset=columnas_criticas)
+
+# columna 'Fuma'
+df['Fuma'] = df['Fuma'].str.lower().replace({
+    'sí': True, 'si': True, 'no': False, 
+    'desconocido': False, 'true': True, 'false': False
+}).astype(bool)
+
+# mes de la fecha
+df['Mes'] = pd.to_datetime(df['Fecha de tamizaje'], format='%d/%m/%Y').dt.month
+
+print(df)
