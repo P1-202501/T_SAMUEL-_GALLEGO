3c173f2ba341de0fecad04e2470a732be68c9f7a
sammygapaCES <gallegop.samuel@uces.edu.co>
2025-05-14 07:43:58 -0500

    fuma

--- Taller.py
+++ Taller.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
 # 1. Cargar el archivo y mostrar primeras filas
-df = pd.read_csv('biometria_pacientes.csv',  encoding='latin-1')
-print(df)
+df = pd.read_csv('biometria_pacientes.csv', sep=';', encoding='latin-1')
+
+
+#2. Identificar duplicados, nulos y valores únicos
+# Dupli
+print(f"Número de duplicados:{df.duplicated().sum()}")
+
+# Nulos por colum
+print(df.isnull().sum())
+# Valores únicos en 'Fuma'
+print(df['Fuma'].unique())
