ae78f673a65d0db0547a27252130155a9e224a10
sammygapaCES <gallegop.samuel@uces.edu.co>
2025-05-14 16:22:07 -0500

    PARTE 2

--- Taller.py
+++ Taller.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+"""PARTE 1"""
 # 1. Cargar el archivo y mostrar primeras filas
 df = pd.read_csv('biometria_pacientes.csv', sep=';', encoding='latin-1')
 
@@ -17,16 +18,49 @@ print(df['Fuma'].unique())
 # 3. eliminar duplicados por columnas criticas
 df = df.drop_duplicates()
 
-columnas_criticas = ['Peso', 'Talla', 'Glucosa', 'Colesterol']
+columnas_criticas = ['Peso' ,'Talla','Glucosa','Colesterol']
 df = df.dropna(subset=columnas_criticas)
 
 # columna 'Fuma'
 df['Fuma'] = df['Fuma'].str.lower().replace({
-    'sí': True, 'si': True, 'no': False, 
-    'desconocido': False, 'true': True, 'false': False
+    'sí': True,'si':True, 'no':False, 
+    'desconocido':False,'true':True,'false' :False
 }).astype(bool)
 
 # mes de la fecha
 df['Mes'] = pd.to_datetime(df['Fecha de tamizaje'], format='%d/%m/%Y').dt.month
+
+
+
+"""PARTE 2"""
+
+#  IMC
+df['IMC'] = df['Peso'] / (df['Talla']/100)**2
+
+#clasific IMC
+def clasifica_imc(imc) :
+    if imc<18.5 :
+        return 'bajo peso'
+    elif 18.5<= imc< 25 :
+         return 'Normal'
+    elif 25<=  imc< 30:
+        return'sobrepeso'
+    else:
+         return 'obesidad'
+
+df['Clasificacion_IMC'] = df['IMC'].apply(clasifica_imc)
+
+#  binaris
+df['Sedentario'] = df['Actividad física (min/sem)'] < 60
+df['Hipertenso'] = (df['PAS'] >= 140) | (df['PAD'] >= 90)
+
+#alterado
+condiciones= [
+    (df['Glucosa']> 126 ),
+    (df['Colesterol' ]> 240 ) ,
+    (df['IMC']> 30 ) ,
+    df['Sedentario' ]
+]
+df['Metabolicamente_alterado'] = sum(condiciones) >=2
 
 print(df)
